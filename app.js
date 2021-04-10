cc86e70820f198198e0bed4c35e685cd49bacd54
Drew Bidmead <dmbidmead@gmail.com>
2021-04-10 12:00:47 -0400

    First piece drawn.

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const CANVAS = document.getElementById('canvas');
 const CTX = CANVAS.getContext('2d');
 const SQUARE_WIDTH = CANVAS.width / 8;
+const PIECE_PADDING = 5;
+const PIECE_WIDTH = SQUARE_WIDTH - 2 * PIECE_PADDING;
+let img = new Image();
+img.src = './img/queen_b.png';
+img.onload = function() {
+  CTX.drawImage(img, (3 * SQUARE_WIDTH) + PIECE_PADDING, PIECE_PADDING, PIECE_WIDTH, PIECE_WIDTH);
+}
 
 class Board {
   constructor() {
