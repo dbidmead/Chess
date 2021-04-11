7513c3264cc37ee642f7be00bcbe3840bf6e2955
Drew Bidmead <dmbidmead@gmail.com>
2021-04-11 01:19:00 -0400

    Rules file instantiated with vision rules object to contain functions that will identify moves for each piece.

---
+++ rules.js
@@ -0,0 +1,31 @@
+var VisionRules = {
+// for all: consider collisions with walls, friendly pieces, and/or opposing pieces!
+  pawnFirst: function() {
+
+  },
+
+  pawnOther: function() {
+
+  },
+
+  pawnCapture: function() {
+
+  },
+
+  straightBeam: function() {
+    //consider castles if rook
+  },
+
+  diagBeam: function() {
+
+  },
+
+  knight: function() {
+
+  },
+
+  king: function() {
+    //consider castles
+  }
+
+}
